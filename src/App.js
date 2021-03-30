210be4a40d4fc5b1eeb8c9be0674654ab3340dc0
Eddy Cedeño <miguepoke28@gmail.com>
2021-03-30 15:19:09 -0500

    Add react and react-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-const Pet = ({name, animal, breed}) => {
+import React from "react";
+import { render } from "react-dom";
+
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
@@ -11,10 +14,10 @@ const App = () => {
     "div",
     {},
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {name: "Mao", animal: "Cat", breed: "idk"}),
-    React.createElement(Pet, {name: "Lune", animal: "Dog", breed: "idk"}),
-    React.createElement(Pet, {name: "May", animal: "Usagi", breed: "idk"})
+    React.createElement(Pet, { name: "Mao", animal: "Cat", breed: "idk" }),
+    React.createElement(Pet, { name: "Lune", animal: "Dog", breed: "idk" }),
+    React.createElement(Pet, { name: "May", animal: "Usagi", breed: "idk" })
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
