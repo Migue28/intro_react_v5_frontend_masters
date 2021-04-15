f3b35679ab11a6bfe27653c60f2a186faa920ce7
Eddy Cedeño <miguepoke28@gmail.com>
2021-04-15 12:39:33 -0500

    Add Details component Add Router to Details and SearchParams

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import React from "react";
 import { render } from "react-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import {Router, Link} from '@reach/router';
+import Details from "./Details";
 
 const App = () => {
-  // return React.createElement(
-  //   "div",
-  //   {},
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, { name: "Mao", animal: "Cat", breed: "idk" }),
-  //   React.createElement(Pet, {
-  //     name: "Lune",
-  //     animal: "Dog",
-  //     breed: "Labrador",
-  //   }),
-  //   React.createElement(Pet, { name: "May", animal: "Usagi", breed: "idk" })
-  // );
   return(
     <div>
-      <h1>Adopt me!</h1>
-      {/* <Pet name="Mao" animal="Cat" breed="idk"/>
-      <Pet name="Lune" animal="Dog" breed="Labrador"/>
-      <Pet name="May" animal="Usagi" breed="white"/> */}
-      <SearchParams />
+      <header>
+      <Link to='/'>
+        Adopt me!
+      </Link>
+      </header>
+      
+      <Router>
+        <SearchParams path='/'/>
+        <Details path='/details/:id'/>
+      </Router>
     </div>
   )
 };
